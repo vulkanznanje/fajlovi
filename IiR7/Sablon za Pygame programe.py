923fcc17df47c25a35825d610c39b812bc02bc49
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2020-04-01 09:20:13 +0200

    Add files via upload

---
+++ IiR7/Sablon za Pygame programe.py
@@ -0,0 +1,21 @@
+import pygame as pg
+pg.init()
+prozor = pg.display.set_mode((300,300))
+pg.display.set_caption("Naslov")
+prozor.fill(pg.Color("White"))
+
+#FUNKCIJE
+#pg.draw.line(prozor, pg.Color("boja"), True/False, (koordinate temena), debljina)
+#pg.draw.lines(prozor, pg.Color("boja"), (x, y), (x, y), debljina)
+#pg.draw.rect(prozor, pg.Color("boja"), (x, y, a, b), debljina)
+#pg.draw.circle(prozor, pg.Color("boja"), (x, y), r, debljina)
+#pg.draw.ellipse(prozor, pg.Color("boja"), (x, y, a, b), debljina)
+#pg.draw.arc(prozor, pg.Color("boja"), (x, y, a, b), ugaoOD, ugaoDO, debljina)
+#pg.draw.polygon(prozor, pg.Color("boja"), (koordinate temena), debljina)
+
+pg.display.update()
+while pg.event.wait().type != pg.QUIT:
+    pass
+pg.quit()
+
+
