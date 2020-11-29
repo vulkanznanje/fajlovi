d966309bb54d65d2093772afeb20ad3a8e411b8d
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2020-11-29 11:12:05 +0100

    Add files via upload

---
+++ Informatika i računarstvo 6/Resenja zadataka iz blokova VEZBAJ/Vezbaj_str141_dvcifrenBroj.py
@@ -0,0 +1,5 @@
+Dvocifren_broj = int(input("Unesi dvocifren broj: "))
+Prva_cifra = (Dvocifren_broj // 10)
+Druga_cifra = (Dvocifren_broj % 10)
+print("Prva cifra je:", Prva_cifra)
+print("Druga cifra je:", Druga_cifra)
