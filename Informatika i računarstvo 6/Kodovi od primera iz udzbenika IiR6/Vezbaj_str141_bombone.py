642d0a27f09a96af1795f60990010cde539bb363
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2020-11-29 16:36:08 +0100

    Delete Vezbaj_str141_bombone.py

--- Informatika i računarstvo 6/Kodovi od primera iz udzbenika IiR6/Vezbaj_str141_bombone.py
+++
@@ -1,8 +0,0 @@
-broj_učenika = int(input("Koliko je učenika prisutno u školi? "))
-broj_bombona = int(input("Koliko je bombona u kesici? "))
-
-bombona_po_učeniku = broj_bombona // broj_učenika
-ostalo_bombona = broj_bombona % broj_učenika
-
-print("Svaki učenik će dobiti", bombona_po_učeniku, "bombona.")
-print("Ostalo je još", ostalo_bombona, "bombona.")
