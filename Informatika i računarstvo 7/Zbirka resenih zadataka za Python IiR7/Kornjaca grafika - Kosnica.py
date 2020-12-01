ae60241298b14018cc3ad0884b87c9d27d42a669
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2020-12-01 16:00:53 +0100

    Add files via upload

---
+++ Informatika i računarstvo 7/Zbirka resenih zadataka za Python IiR7/Kornjaca grafika - Kosnica.py
@@ -0,0 +1,32 @@
+#Napiši program koji će nacrtati tri pravilna šestougla
+#različite boje #koji će imati po jednu zajedničku stranicu.
+
+#Nakon toga pomoću brojačke petlje skrati programski kod.
+
+#Ako ti to pođe za rukom, nastavi da crtaš "košnicu".
+
+import turtle
+turtle.width(5)
+turtle.color("brown")
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.rt(-120)
+turtle.color("blue")
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.rt(-120)
+turtle.color("green")
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
+turtle.fd(100);turtle.rt(60)
