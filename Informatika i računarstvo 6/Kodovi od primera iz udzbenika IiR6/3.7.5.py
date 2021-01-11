7cb8b372a0828505243588bfa6e2df05832e844b
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2021-01-11 14:27:56 +0100

    Delete 3.7.5.py

--- Informatika i računarstvo 6/Kodovi od primera iz udzbenika IiR6/3.7.5.py
+++
@@ -1,8 +0,0 @@
-import turtle
-turtle.shape("turtle")
-turtle.width(5)
-turtle.color("red")
-turtle.fd(100);turtle.rt(90)
-turtle.fd(100);turtle.rt(90)
-turtle.fd(100);turtle.rt(90)
-turtle.fd(100);turtle.rt(90)
