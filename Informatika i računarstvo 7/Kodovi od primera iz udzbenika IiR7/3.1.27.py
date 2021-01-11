035bc1566292444ed5a4906555cb437f09c18e7c
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2021-01-11 15:23:29 +0100

    Delete 3.1.27.py

--- Informatika i računarstvo 7/Kodovi od primera iz udzbenika IiR7/3.1.27.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Unesi početak intervala: "))
-m = int(input("Unesi kraj intervala: "))
-Zbir = 0
-for i in range(n, m+1):
-    Zbir = Zbir + i
-    print(Zbir)
-print("Zbir celih brojeva u navedenom intervalu je:", Zbir)
-
