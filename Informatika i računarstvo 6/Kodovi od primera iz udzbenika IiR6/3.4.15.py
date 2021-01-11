b078292a3d316ae4739974a0a170ad0faa5966a1
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2021-01-11 14:26:24 +0100

    Delete 3.4.15.py

--- Informatika i računarstvo 6/Kodovi od primera iz udzbenika IiR6/3.4.15.py
+++
@@ -1,8 +0,0 @@
-Cena = 70
-print("Cena sladoleda je:", Cena)
-#sladoled je poskupeo 10 posto.
-Cena = Cena + Cena*10/100
-print("Cena sladoleda je:", Cena)
-#sladoled je pojeftinio 10 posto! :)
-Cena = Cena - Cena*10/100
-print("Cena sladoleda je:", Cena)
