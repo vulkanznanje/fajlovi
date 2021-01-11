0c0cf9a879e1a3f23483dcea88022c999cbcf0d3
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2021-01-11 14:30:21 +0100

    Delete 3.9.11.py

--- Informatika i računarstvo 6/Kodovi od primera iz udzbenika IiR6/3.9.11.py
+++
@@ -1,6 +0,0 @@
-Polugodište = float(input("Unesi prosek na kraju I polugodišta: "))
-Kraj = float(input("Unesi prosek na kraju VI razreda: "))
-if (Polugodište == 5.00 or Kraj == 5.00):
-    print ("Dobijam nov bicikl!")
-else:
-    print ("Nisam ispunio/la sve uslove...")
