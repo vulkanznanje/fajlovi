0de5ad7fa366863fae73e57e58803e12b85fad38
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2021-01-11 14:31:43 +0100

    Delete str173 pitanje 4.py

--- Informatika i računarstvo 6/Kodovi od primera iz udzbenika IiR6/str173 pitanje 4.py
+++
@@ -1,9 +0,0 @@
-if not (Odgovor == "P" or Odgovor == "K" or Odgovor == "T"):
-    print ("Uneo/la si pogrešno slovo!")
-
-if not (Odgovor = "P" or Odgovor = "K" or Odgovor = "T"):
-    print ("Uneo/la si pogrešno slovo!")
-
-if not (Odgovor == "P" and Odgovor == "K" and Odgovor == "T"):
-    print ("Uneo/la si pogrešno slovo!")
-
