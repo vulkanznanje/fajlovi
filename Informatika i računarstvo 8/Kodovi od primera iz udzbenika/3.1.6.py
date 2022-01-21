bf5c252ab314422c60867096a7364ca98e2af908
Vulkan znanje <61086486+vulkanznanje@users.noreply.github.com>
2022-01-21 13:22:04 +0100

    Delete Informatika i računarstvo 8/Kodovi od primera iz udzbenika directory

--- Informatika i računarstvo 8/Kodovi od primera iz udzbenika/3.1.6.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-Učenici = ["Una", "Marta", "Ognjen", "Petra",
-"Novak", "Đurđe", "Kalina", "Vanja"]
-Čučnjevi = [25, 16, 5, 21, 28, 25, 25, 26]
-plt.bar(Učenici, Čučnjevi, color = "r")
-plt.title("Čučnjevi")
-plt.show()
-plt.close()
-
